129b1edd92a93e4ea37b61a2b09cc963510fceb5
Exilic <tobias.engberg@exilic.se>
2022-06-22 12:17:15 +0200

    Code along June 22

---
+++ Conjugation/Program.cs
@@ -0,0 +1,33 @@
+﻿using Conjugation;
+
+// futur
+// conditionnal
+// passé composé
+
+// Parl-er
+// je parl-erai
+// je parl-erais
+// j'ai parl-é
+
+// Fini-r
+// fini-rai
+// fini-rais
+// fini
+
+// Vend-re
+// vend-rai
+// vend-rais
+// vend-u
+
+
+
+
+
+string tenseChosen = Utilities.GetVerbTense();
+string enteredWord = Utilities.GetVerb();
+string verbKind = Utilities.GetVerbKind(enteredWord);
+string finalResult = Utilities.GetFinalResult(tenseChosen, enteredWord);
+Utilities.OutputFinalResult(finalResult, verbKind);
+
+
+
---
+++ Conjugation/Utilities.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Conjugation
+{
+	public class Utilities
+	{
+		public Utilities()
+		{
+		}
+
+		public static string GetVerbKind(string wordToCheck)
+		{
+			string verbKind = "";
+			if (wordToCheck.EndsWith("er"))
+			{
+				verbKind = "the first";
+			}
+			else if (wordToCheck.EndsWith("ir"))
+			{
+				verbKind = "the second";
+			}
+			else if (wordToCheck.EndsWith("re"))
+			{
+				verbKind = "the third";
+			}
+			else
+			{
+				verbKind = "no";
+			}
+
+			return verbKind;
+		}
+
+		public static string GetVerbTense()
+        {
+			string tenseChosen;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Choose a verb tense from the list:");
+				Console.WriteLine("1. futur (press 1)");
+				Console.WriteLine("2. conditionnel (press 2)");
+				Console.WriteLine("3. passé composé (press 3)");
+				tenseChosen = Console.ReadKey().KeyChar.ToString();
+
+			} while (tenseChosen != "1" && tenseChosen != "2" && tenseChosen != "3");
+			Console.Clear();
+
+			return tenseChosen;
+		}
+
+		public static string GetVerb()
+        {
+			Console.WriteLine("Please enter a regular verb in french:");
+			return Console.ReadLine();
+		}
+
+		public static string GetFinalResult(string tenseChosen, string enteredWord)
+        {
+			string verbTense = tenseChosen switch
+			{
+				"1" => "futur",
+				"2" => "conditionnel",
+				"3" => "passé composé",
+			};
+
+			int lettersToCut = tenseChosen switch
+			{
+				"1" => 2,
+				"2" => 1,
+				"3" => 2,
+			};
+
+			string verbBase = Utilities.GetFoundation(enteredWord, lettersToCut);
+
+
+			string finalOutput = $"The right verb form for {verbTense} of {enteredWord} is {verbBase}";
+			return finalOutput;
+        }
+
+		public static string GetFoundation(string verb, int lettersToCut)
+        {
+			int fromEnd = verb.Length - lettersToCut;
+			return verb.Remove(fromEnd);
+        }
+
+		public static string GetFirstPersonFormat()
+        {
+			string firstPersonForm = "";
+			return firstPersonForm;
+        }
+
+		public static void OutputFinalResult(string finalOutput, string verbKind)
+        {
+			Console.Clear();
+			Console.WriteLine($"The verb is of {verbKind} kind");
+			Console.WriteLine(finalOutput);
+			Console.ReadKey();
+		}
+
+			
+	}
+}
+
